2204e7a51a4c93112ef363f95a829944bf19cad8
Hoang Viet <51878941+TheMarvelousWhale@users.noreply.github.com>
2020-10-09 16:22:26 +0800

    Update Logic for LEDs and Heart rate formula

--- sketch_oct09a.ino
+++ sketch_oct09a.ino
@@ -15,14 +15,32 @@ void setup() {
 int sig;
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
   sig=analogRead(A0);
   Serial.println(sig);
-  if (sig > 945)
-    buzz();
-    delay(400);
 
-  
+  int const thres = 945;
+  int rounds = 10;
+  int lastTime =0,tic, period;
+  for (int nani =0; nani < rounds; nani++) {
+  if (sig>thres )
+    {
+    while(sig>thres) ;
+    buzz();
+    tic = millis() ;
+    if (lastTime!=0)
+       period = period+ (tic-lastTime) ;
+    lastTime = tic;
+    } 
+  } 
+   
+  int heartrate = (rounds-1)*1000*60/period;
+  if (heartrate >=100 && heartrate <160 )  
+     OnOnlyThisLED(YellowLED) ;
+  else if (heartrate >= 40 && heartrate <100) 
+     OnOnlyThisLED(GreenLED) ;
+  else 
+     ONonlyThisLED(RedLED) ;
 }
 
 void buzz() {
@@ -32,7 +50,13 @@ void buzz() {
   }
 
 
-void blinkLED(int pin) {
-  
+void offAllLEDs() {
+  digitalWrite(GreenLED, LOW);
+  digitalWrite(YellowLED, LOW) ;
+  digitalWrite(GreenLED, LOW) ;
   }
-  
+
+void OnOnlyThisLED(int pin) {
+  offAllLEDs() ;
+  digitalWrite(pin, HIGH) ;
+} 
